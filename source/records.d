e6b14f2d212627b3bb9fe29bf437fe75481599de
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-01-06 13:49:02 +0700

    latest filled fix

--- source/records.d
+++ source/records.d
@@ -21,7 +21,7 @@ immutable half_block_duration_hours = 12;
 uint CalcLatestFilledBlockNum()
 {
     uint hours = to!uint(Clock.currTime.toUnixTime / 3600);
-    return hours / half_block_duration_hours;
+    return hours / half_block_duration_hours - 2;
 }
 
 unittest
