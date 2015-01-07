331dfed6cdb860012db51fc2aa2feaa8886d7028
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-01-07 08:15:27 +0700

    record struct improved

--- source/records.d
+++ source/records.d
@@ -2,6 +2,9 @@
 
 import std.datetime;
 import std.conv;
+import std.digest.sha;
+import std.typecons;
+
 
 enum ChainType
 {
@@ -9,16 +12,24 @@ enum ChainType
     Test
 }
 
+alias BlockHash = Typedef!SHA1;
+alias Signature = Typedef!ubyte[10];
+alias PoW = Typedef!ubyte[10];
+
 struct Record
 {
     ChainType chainType;
     ubyte[] key;
     ubyte[] value;
+    Signature signature;
+    uint blockNum;
+    BlockHash prevFilledBlock;
+    PoW proofOfWork;
 }
 
 immutable half_block_duration_hours = 12;
 
-uint CalcLatestFilledBlockNum()
+uint CalcCurrentFilledBlockNum()
 {
     uint hours = to!uint(Clock.currTime.toUnixTime / 3600);
     return hours / half_block_duration_hours - 2;
@@ -26,5 +37,5 @@ uint CalcLatestFilledBlockNum()
 
 unittest
 {
-    assert(CalcLatestFilledBlockNum > 32800);
+    assert(CalcCurrentFilledBlockNum > 32800);
 }
