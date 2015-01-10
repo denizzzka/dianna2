9d4ef40b8c9bd3567afa38fb6f8bc5d289eeeb5b
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-01-11 04:30:31 +0700

    generation.d

---
+++ source/generation.d
@@ -0,0 +1,136 @@
+@trusted:
+
+import records;
+import core.time;
+import std.concurrency;
+import std.random: uniform;
+import core.cpuid: threadsPerCPU;
+debug(PoWt) import std.stdio; // PoWt == "PoW threads"
+
+
+void calcPowForRecord(ref Record r)
+{
+    immutable threads = threadsPerCPU;
+    bool isFound;
+    
+    do
+    {
+        immutable _r = cast(immutable Record) r;
+        
+        isFound = calcPowWithTimeout(_r, dur!"seconds"(10), threads, r.proofOfWork);
+    }
+    while(!isFound);
+}
+
+bool calcPowWithTimeout(
+    immutable Record r,
+    immutable Duration duration,
+    immutable size_t threadsNum,
+    out PoW pow
+)
+{
+    Tid[] children;
+    
+    debug(PoWt) writeln("Start workers");
+    foreach(i; 0..threadsNum)
+        children ~= spawn(&worker, r);
+    
+    debug(PoWt) writeln("Wait for any child why solved PoW");
+    bool isFound = receiveTimeout(duration,
+        (PoW _pow){ pow = _pow; }
+    );
+    
+    debug(PoWt) writeln("Sending 'stop' for all threads");
+    foreach(ref c; children)
+        send(c, true);
+    
+    debug(PoWt) writeln("Wait for children termination");
+    foreach(i; 0..children.length)
+    {
+        receive(
+            (ubyte){}
+        );
+        
+        /*
+         * mbox also can contain other "solved" messages from any
+         * another lucky thread - it is need to receive it too
+         */
+        receiveTimeout(dur!"seconds"(0),
+            (PoW _pow){ pow = _pow; isFound = true; }
+        );
+        
+        debug(PoWt) writeln("Child ", i, " terminated");
+    }
+    
+    return isFound;
+}
+
+private void worker(immutable Record r)
+{
+    debug(PoWt) auto id = "(no id)";
+    debug(PoWt) writeln("Worker ", id, " thread started for Record.key=", r.key);
+    
+    for(auto i = 1;; i++)
+    {
+        debug(PoWt) writeln("Worker ", id, " iteration: ", i);
+        
+        // "close this thread" message received?
+        Duration dur;
+        if(receiveTimeout(dur, (bool){}))
+            break;
+        
+        // Generate random salt
+        PoW pow;
+        foreach(ref e; pow.salt)
+            e = uniform!ubyte;
+        
+        pow.hash = calcPoWHash(r.calcHash, pow.salt);
+        
+        if(isSatisfyDifficulty(pow.hash, r.difficulty))
+        {
+            debug(PoWt) writeln("PoW solved, worker ", id, ", proofOfWork=", pow);
+            
+            send(ownerTid(), pow);
+            break;
+        }
+    }
+    
+    // message what means "child is stopped"
+    send(ownerTid(), cast(ubyte) 0xED);
+}
+
+void benchmark()
+{
+    import std.stdio;
+    import std.datetime;
+    
+    
+    immutable threads = threadsPerCPU();
+    immutable hashesPerThread = 10;    
+    immutable hashes = hashesPerThread * threads;
+    
+    StopWatch sw;
+    
+    writeln("Starting benchmarking");
+    writeln("Hashes: ", hashes, ", threads: ", threads);
+    
+    sw.start();
+    
+    foreach(n; 1..hashesPerThread)
+    {
+        immutable Record r;
+        PoW pow;
+        
+        calcPowWithTimeout(r, dur!"days"(365*10), threads, pow);
+    }
+    
+    sw.stop();
+    
+    writeln("Hashes per minute: ", (cast(float) hashes) / sw.peek.seconds * 60);
+    writeln("Elapsed time, minutes: ", cast(float) sw.peek.seconds / 60);
+}
+
+unittest
+{
+    benchmark();
+}
--- source/work.d
+++ source/work.d
@@ -2,10 +2,9 @@
 
 import storage;
 import records;
-import std.concurrency;
+import generation;
 import core.time: Duration, dur;
-import std.random;
-debug(PoWt) import std.stdio; // PoWt is "PoW threads"
+debug(PoW) import std.stdio;
 
 
 void createNewRecord(Storage s, ubyte[] key, ubyte[] value)
@@ -20,7 +19,7 @@ void createNewRecord(Storage s, ubyte[] key, ubyte[] value)
     s.addRecordAwaitingPoW(r);
 }
 
-void calcPowForNewRecords(Storage s, ChainType chainType, size_t threadsNum) @trusted
+void calcPowForNewRecords(Storage s, ChainType chainType) @trusted
 {
     Record[] records;
     
@@ -28,107 +27,17 @@ void calcPowForNewRecords(Storage s, ChainType chainType, size_t threadsNum) @tr
     {
         records = s.getOldestRecordsAwaitingPoW(chainType, 1);
         
-        debug(PoWt) writeln("Got ", records.length, " record(s) awaiting PoW");
+        debug(PoW) writeln("Got ", records.length, " record(s) awaiting PoW");
         
         if(records.length == 0) return;
         assert(records.length == 1);
         
         records[0].difficulty = 0xDFFFFFFFFFFFFFFF;
-        calcPowForRecord(records[0], threadsNum);
+        calcPowForRecord(records[0]);
         
         s.setCalculatedPoW(records[0]);
     }
     while(records.length > 0);
-}
-
-void calcPowForRecord(ref Record r, inout size_t threadsNum) @trusted
-{
-    bool isFound;
-    
-    do
-    {
-        immutable _r = cast(immutable Record) r;
-        
-        isFound = calcPowWithTimeout(_r, dur!"seconds"(10), threadsNum, r.proofOfWork);
-    }
-    while(!isFound);
-}
-
-bool calcPowWithTimeout(
-    immutable Record r,
-    immutable Duration duration,
-    immutable size_t threadsNum,
-    out PoW pow
-) @trusted
-{
-    Tid[] children;
-    
-    debug(PoWt) writeln("Start workers");
-    foreach(i; 0..threadsNum)
-        children ~= spawn(&worker, r);
-    
-    debug(PoWt) writeln("Wait for any child why solved PoW");
-    bool isFound = receiveTimeout(duration,
-        (PoW _pow){ pow = _pow; }
-    );
-    
-    debug(PoWt) writeln("Sending 'stop' for all threads");
-    foreach(ref c; children)
-        send(c, true);
-    
-    debug(PoWt) writeln("Wait for children termination");
-    foreach(i; 0..children.length)
-    {
-        receive(
-            (ubyte){}
-        );
-        
-        /*
-         * mbox also can contain other "solved" messages from any
-         * another lucky thread - it is need to receive it too
-         */
-        receiveTimeout(dur!"seconds"(0),
-            (PoW _pow){ pow = _pow; isFound = true; }
-        );
-        
-        debug(PoWt) writeln("Child ", i, " terminated");
-    }
-    
-    return isFound;
-}
-
-private void worker(immutable Record r) @trusted
-{
-    debug(PoWt) auto id = "(no id)";
-    debug(PoWt) writeln("Worker ", id, " thread started for Record.key=", r.key);
-    
-    for(auto i = 1;; i++)
-    {
-        debug(PoWt) writeln("Worker ", id, " iteration: ", i);
-        
-        // "close this thread" message received?
-        Duration dur;
-        if(receiveTimeout(dur, (bool){}))
-            break;
-        
-        // Generate random salt
-        PoW pow;
-        foreach(ref e; pow.salt)
-            e = uniform!ubyte;
-        
-        pow.hash = calcPoWHash(r.calcHash, pow.salt);
-        
-        if(isSatisfyDifficulty(pow.hash, r.difficulty))
-        {
-            debug(PoWt) writeln("PoW solved, worker ", id, ", proofOfWork=", pow);
-            
-            send(ownerTid(), pow);
-            break;
-        }
-    }
-    
-    // message what means "child is stopped"
-    send(ownerTid(), cast(ubyte) 0xED);
 }
 
 unittest
@@ -142,42 +51,7 @@ unittest
     auto r = s.getOldestRecordsAwaitingPoW(ChainType.Test, 2);
     assert(r.length == 2);
     
-    s.calcPowForNewRecords(ChainType.Test, 3);
+    s.calcPowForNewRecords(ChainType.Test);
     
     s.purge;
-    
-    benchmark();
-}
-
-void benchmark() @trusted
-{
-    import std.stdio;
-    import std.datetime;
-    import core.cpuid;
-    import std.random;
-    
-    
-    immutable threads = threadsPerCPU();
-    immutable hashesPerThread = 10;    
-    immutable hashes = hashesPerThread * threads;
-    
-    StopWatch sw;
-    
-    writeln("Starting benchmarking");
-    writeln("Hashes: ", hashes, ", threads: ", threads);
-    
-    sw.start();
-    
-    foreach(n; 1..hashesPerThread)
-    {
-        immutable Record r;
-        PoW pow;
-        
-        calcPowWithTimeout(r, dur!"days"(365*10), threads, pow);
-    }
-    
-    sw.stop();
-    
-    writeln("Hashes per minute: ", (cast(float) hashes) / sw.peek.seconds * 60);
-    writeln("Elapsed time, minutes: ", cast(float) sw.peek.seconds / 60);
 }
