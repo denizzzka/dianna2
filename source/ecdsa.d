505523c4af959a610162fb7e6457f2b7a7369c91
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-02-07 23:11:35 +0700

    EC key generation works

--- source/ecdsa.d
+++ source/ecdsa.d
@@ -1,12 +1,14 @@
 @trusted:
 
-import deimos.openssl.ec: NID_secp256k1;
+import deimos.openssl.evp;
 import deimos.openssl.ecdsa;
+
+import std.exception: enforce;
 
 
 struct Key
 {
-    typeof(EC_KEY_new_by_curve_name(NID_secp256k1)) key;
+    ubyte[30] key;
     alias key this;
     
     string name;
@@ -16,11 +18,37 @@ alias signature = ECDSA_SIG;
 
 Key newKey(in string name)
 {
-    Key r;
-    r.name = name;
-    r = EC_KEY_new_by_curve_name(NID_secp256k1);
+    Key res;
+    res.name = name;
+    //res.key = EC_KEY_new_by_curve_name(NID_secp256k1);
     
-    return r;
+    EVP_PKEY_CTX* pctx;
+    EVP_PKEY_CTX* kctx;
+    EVP_PKEY* params;
+    EVP_PKEY* key;
+    
+    pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, null);
+    enforce(pctx);
+    
+    enforce(EVP_PKEY_paramgen_init(pctx));
+    
+    enforce(EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, NID_secp256k1));
+    
+    enforce(EVP_PKEY_paramgen(pctx, &params));
+    
+    kctx = EVP_PKEY_CTX_new(params, null);
+    enforce(kctx);
+    
+    enforce(EVP_PKEY_keygen_init(kctx));
+    
+    enforce(EVP_PKEY_keygen(kctx, &key));
+    
+    // FIXME: need exit scope
+    if(pctx) EVP_PKEY_CTX_free(pctx);
+    if(kctx) EVP_PKEY_CTX_free(kctx);
+    if(params) EVP_PKEY_free(params);
+    
+    return res;
 }
 
 unittest
