16f8c9b2eff820a9fbe54cb3ac63caa99c6f3d90
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-04-12 23:11:49 +0700

    reading config

---
+++ source/config.d
@@ -0,0 +1,31 @@
+import inifiled;
+import std.path: expandTilde;
+import std.exception;
+
+
+@INI("Main section")
+private struct Config {
+    @INI("Local addresses. IP and IPv6 is allowed, port is required."~
+	 "Example: :::60180 - bind to all local addresses and port 60180")
+    string[] listen_addresses = [":::60180", ":::60181"];
+}
+
+private Config _cfg;
+
+ref const (Config) cfg()
+{
+    return _cfg;
+}
+
+static this()
+{
+    try
+	readINIFile(_cfg, "/etc/dianna2.conf");
+    catch(ErrnoException e)
+    {}
+    
+    try
+	readINIFile(_cfg, "~/.config/dianna2/dianna2.conf");
+    catch(ErrnoException e)
+    {}
+}
