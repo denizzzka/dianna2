856bd1aabe0046e5e4ee280b7771abc2cc9422c3
Denis Feklushkin <denis.feklushkin@gmail.com>
2015-04-13 17:44:08 +0700

    UPnP discovery failed case

--- source/net.d
+++ source/net.d
@@ -8,20 +8,26 @@ import miniupnpc.upnpcommands;
 
 unittest
 {
-    UPNPDev* upnp;
     int err;
-    upnp = enforce(upnpDiscover(2000, null, null, false, false, &err));
-
+    UPNPDev* upnp = upnpDiscover(2000, null, null, false, false, &err);
+    
     scope(exit) if(upnp) freeUPNPDevlist(upnp);
-
-    UPNPUrls urls;
-    IGDdatas data;
-    char[32] lanaddr;
-    UPNP_GetValidIGD(upnp, &urls, &data, lanaddr.ptr, lanaddr.length.to!int);
-
-    char[32] addr;
-    UPNP_GetExternalIPAddress(urls.controlURL, data.first.servicetype.ptr, addr.ptr);
-
-    writeln("LAN Address : ", lanaddr.ptr.to!string);
-    writeln("External IP Address : ", addr.ptr.to!string);
+    
+    if(!upnp)
+    {
+        writeln("UPnP discovery failed");
+    }
+    else
+    {
+        UPNPUrls urls;
+        IGDdatas data;
+        char[32] lanaddr;
+        UPNP_GetValidIGD(upnp, &urls, &data, lanaddr.ptr, lanaddr.length.to!int);
+        
+        char[32] addr;
+        UPNP_GetExternalIPAddress(urls.controlURL, data.first.servicetype.ptr, addr.ptr);
+        
+        writeln("LAN Address : ", lanaddr.ptr.to!string);
+        writeln("External IP Address : ", addr.ptr.to!string);
+    }
 }
